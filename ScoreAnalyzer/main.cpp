1b3517c9db7d12492fdb74a75f6b7ebbb55e7662
Albin <albin.lorentzson@me.com>
2017-02-11 10:26:08 +0100

    first commit

---
+++ ScoreAnalyzer/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  ScoreAnalyzer
+//
+//  Created by Albin Lorentzson on 2017-02-11.
+//  Copyright © 2017 Albin Lorentzson. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
